904297e0552826f624c186f0e885cbc87cf2d290
Joanna Bell <jjbdigital@gmail.com>
2022-10-18 10:21:53 -0500

    commit tree 8da33027ed6fab9cd5bd1eaf5af6316dfbbf1db7

--- app/models/artist.rb
+++ app/models/artist.rb
@@ -1,16 +1,28 @@
 class Artist < ActiveRecord::Base
+  # an artist has many songs
   has_many :songs
+
+  # an artist has many songs in it's genre
   has_many :genres, through: :songs
+
+  def get_first_song
+      #return the artist's first saved song
+    self.songs.first
+  end
+
 
   def get_genre_of_first_song
     #return the genre of the artist's first saved song
+    self.genres.first
   end
 
   def song_count
-    #return the number of songs associated with the artist
+    #return the total number of songs associated with the artist
+    self.songs.count
   end
 
   def genre_count
-    #return the number of genres associated with the artist
+    #return the total number of genres associated with the artist
+    self.genres.count 
   end
 end
--- app/models/genre.rb
+++ app/models/genre.rb
@@ -1,16 +1,26 @@
 class Genre < ActiveRecord::Base
+  # a genre has many songs
   has_many :songs
+
+  # a genre has many artists
   has_many :artists, through: :songs
 
   def song_count
     # return the number of songs in a genre
+    self.songs.count
   end
 
   def artist_count
     # return the number of artists associated with the genre
+    self.artists.count
   end
 
   def all_artist_names
     # return an array of strings containing every musician's name
+    # must map or pluck
+    # self.artists.pluck(:name)
+    self.artists.map do |t|
+      t.name
+    end
   end
 end
